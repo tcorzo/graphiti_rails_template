ace2ff6bdb645bb54669b2e691e2e758f4470c70
Lee Richmond <lrichmond1@bloomberg.net>
2019-02-03 11:13:23 -0500

    Remove old routes file

--- routes.rb
+++
@@ -1,7 +0,0 @@
-insert_into_file "config/routes.rb", :after => "Rails.application.routes.draw do\n" do
-  <<-STR
-  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
-    # your routes go here
-  end
-  STR
-end
