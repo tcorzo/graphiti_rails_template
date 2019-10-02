489bd778c05ff770073e203c07c558855ff4444d
Casey Provost <provost.design@gmail.com>
2019-10-02 15:39:53 -0400

    Update gems.rb
    Generate the project for Rails 6 by default

--- gems.rb
+++ gems.rb
@@ -1,16 +1,28 @@
+RAILS_ENV = ENV.fetch("RAILS_ENV", "6")
+
 gem 'graphiti'
 gem 'graphiti-rails'
 gem 'vandal_ui'
-gem 'kaminari', '~> 1.0'
-gem 'responders', '~> 2.4'
+gem 'kaminari', '~> 1.1'
+
+if RAILS_ENV.to_i == 5
+  gem 'responders', '~> 2.4'
+else
+  gem 'responders', '~> 3.0'
+end
 
 gem_group :development, :test do
-  gem 'rspec-rails', '~> 3.5.2'
-  gem 'factory_bot_rails', '~> 4.0'
-  gem 'faker', '~> 1.7' # keep here for seeds.rb
+  if RAILS_ENV.to_i == 5
+    gem 'rspec-rails', '~> 3.5.2'
+  else
+    gem 'rspec-rails', '~> 4.0.0beta2'
+  end
+  
+  gem 'factory_bot_rails', '~> 5.0'
+  gem 'faker', '~> 2.5' # keep here for seeds.rb
   gem 'graphiti_spec_helpers'
 end
 
 gem_group :test do
-  gem 'database_cleaner', '~> 1.6'
+  gem 'database_cleaner', '~> 1.7'
 end
